cb8b85bbc6e826918838ccd943975f2db11651c4
Andrii Dats <24232842+fAtsteD@users.noreply.github.com>
2021-06-19 18:05:00 +0300

    Add zsh settings and theme

--- Linux/zsh.sh
+++ Linux/zsh.sh
@@ -5,3 +5,12 @@ sudo apt install -y zsh
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# ZSH settings
+cp "./zsh/.zshrc" "$HOME/.zshrc"
+
+# Install theme p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# p10k settings
+cp "./zsh/.p10k.zsh" "$HOME/.p10k.zsh"
