d15498d874e65ecae03428881cebffa790a0df29
Andrii Dats <24232842+fAtsteD@users.noreply.github.com>
2021-06-27 13:58:31 +0300

    Fix git alias. Delete always sign commits

--- Linux/git.sh
+++ Linux/git.sh
@@ -6,8 +6,6 @@ sudo apt install git git-extras
 echo "Set config git"
 git config --global user.name "Andrii Dats"
 git config --global user.email "4ndrey.d4t5@gmail.com"
-# Always sign the commit
-git config --global commit.gpgsign true
 # The program for signing
 git config --global gpg.program gpg
 # Store login, email, password for accessing to remote repositories
@@ -19,6 +17,6 @@ git config --global alias.fixlast "commit --all --amend --no-edit"
 # Aliass for short added/changes files
 git config --global alias.stsb "status -sb"
 # Aliass for colorful branch graph
-git config --global alias.stsb "log --graph --abbrev-commit --decorate --all --format=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)\""
+git config --global alias.graph "log --graph --abbrev-commit --decorate --all --format=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)\""
 # Aliass for last tag
 git config --global alias.lasttag "describe --tags $(git rev-list --tags --max-count=1)"
